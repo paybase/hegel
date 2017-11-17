08e955663fd758bcf2f8450f446e7ebb9671214b
Joel Pickup <joel@paybase.io>
2017-11-17 17:39:59 +0000

    ghr call

--- build/release.sh
+++ build/release.sh
@@ -9,4 +9,4 @@ zip $filename target/x86_64-unknown-linux-musl/release/hegel
 curl -Lk https://github.com/tcnksm/ghr/releases/download/v0.5.4/ghr_v0.5.4_linux_amd64.zip -o ghr.zip
 unzip ghr.zip
 
-ghr $CIRCLE_TAG $filename
+./ghr $CIRCLE_TAG $filename
