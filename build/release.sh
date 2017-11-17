2092bb69ce9ba38a40c59cb57ac8d326d765971d
Joel Pickup <joel@paybase.io>
2017-11-17 17:58:45 +0000

    fix flag

--- build/release.sh
+++ build/release.sh
@@ -9,4 +9,4 @@ zip $filename target/x86_64-unknown-linux-musl/release/hegel
 curl -Lk https://github.com/tcnksm/ghr/releases/download/v0.5.4/ghr_v0.5.4_linux_amd64.zip -o ghr.zip
 unzip ghr.zip
 
-./ghr -u paybase-ci -repo hegel $CIRCLE_TAG $filename
+./ghr -u paybase-ci -r hegel $CIRCLE_TAG $filename
