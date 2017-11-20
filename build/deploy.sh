fd6d6b6e78834243226ea37cfcdf152bb1e4949b
Joe Harlow <joe@paybase.io>
2017-11-20 12:29:25 +0000

    only build release on deploy

--- build/deploy.sh
+++ build/deploy.sh
@@ -4,5 +4,8 @@ set -e
 if [ -z "$CIRCLE_TAG" ] && [ "$CIRCLE_BRANCH" = master ]; then
   sh -c "build/tag.sh"
 elif [ -n "$CIRCLE_TAG" ]; then
+  cargo build --release
   sh -c "build/release.sh"
+else
+  echo "not doing anything here!"
 fi
